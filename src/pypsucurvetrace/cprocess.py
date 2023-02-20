1f8d0cdcf09575ac92f698eb2d64983a00302040
mbrennwa <matthias@brennwald.org>
2023-02-20 21:59:48 +0100

    added temporary code to prepare for interpolation and differentiation of data

--- src/pypsucurvetrace/cprocess.py
+++ src/pypsucurvetrace/cprocess.py
@@ -21,8 +21,15 @@ from pypsucurvetrace.read_datafile import read_datafile
 from pypsucurvetrace.curvetrace_tools import say_hello
 
 
+
+import numpy as np
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+
+
+
 # set up logger:
-logger = logging.getLogger('extracpreheat')
+logger = logging.getLogger('curveprocess')
 if not logger.handlers:
     logger.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
@@ -41,8 +48,6 @@ def cprocess():
     # main program #
     ################
 
-    raise NotImplementedError('cprocess / curveprocess not yet implemented.')
-
     # input arguments:
     parser = argparse.ArgumentParser(description='curveprocess is a Python program to extract and calculate parameters from from pypsucurvetrace data files.')
     parser.add_argument('datafiles', nargs='+', help='Names (and paths) of pypsucurvetrace data files, can use wildcards.')
@@ -55,9 +60,56 @@ def cprocess():
     # determine data file(s):
     datafiles = args.datafiles
 
+
+
+    logger.warning('cprocess / curveprocess not yet implemented.')
+
+
+
     # extract preheat data from files and print to console:
     sep = ', '
     print( 'Filename' + sep + 'Label' + sep + 'U0' + sep + 'I0' + sep + 'Uc' + sep + 'Ic' + sep + 'T')
     for i in range(len(datafiles)):
-	    d, l, p, u = read_datafile(datafiles[i])
+    
+        # read data file:
+	    d, l, p, R2_val = read_datafile(datafiles[i])
+	    
+	    # print pre-heat data:
 	    print( Path(d.datafile).stem + sep + l + sep + str(p.U0) + sep + str(p.I0) + sep + str(p.Uc) + sep + str(p.Ic) + sep + str(p.T))
+	    
+	    
+	    logger.warning('toying around with grid data and interpolation...')
+	    
+	    # get U1, U2, I1 data:
+	    U1 = d.get_U1_meas(exclude_CC = True)
+	    U2 = d.get_U2_set(exclude_CC = True)
+	    I1 = d.get_I1_meas(exclude_CC = True)
+
+	    # plot raw data:
+	    plt.ion()
+	    fig = plt.figure()
+	    ax = fig.add_subplot(111, projection='3d')
+	    ax.plot_trisurf(U1, U2, I1, color='white', edgecolors='grey', alpha=0.5)
+	    ax.scatter(U1, U2, I1, c='red')
+	    plt.show()
+
+	    # interpolate:
+	    u1 = np.linspace(U1.min(), U1.max(), 1000)
+	    u2 = np.linspace(U2.min(), U2.max(), 1000)
+	    UU1, UU2 = np.meshgrid(u1,u2)
+	    II1 = griddata((U1, U2), I1, (UU1, UU2), method='cubic')
+
+	    # plot interpolated data (contours):
+	    fig, ax = plt.subplots(nrows=1, ncols=1)
+	    ax.contour(UU1, UU2, II1)
+	    plt.ioff()
+	    plt.show()
+
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
